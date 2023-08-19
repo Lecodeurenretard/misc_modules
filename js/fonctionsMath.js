cfa1eaaa36222f06a3d0e0903a1da2e2ff398de1
LeRetardatN <111014383+Lecodeurenretard@users.noreply.github.com>
2023-08-19 16:04:41 +0200

    apport classes 'Mathematics' et 'Fraction' avec leurs différentes méthodes.

---
+++ js/fonctionsMath.js
@@ -0,0 +1,194 @@
+class Mathemathics{
+    /**
+    * Convertit les degrés en radians.
+    * @param {Number} angle number L'angle devant être convertit (doit être en degré).
+    * @param {Boolean} coef Si true, la fonction ne retournera que le coefficient. On peut donc écrire pour tout a (avec la fonction représentée par f()): f(a) = π*f(a, true)
+    * @returns {Number}  L'équivalent en radian.
+    */
+    static ConvertToRadian(angle, coef=false){
+        if (coef) {
+            angle *= Math.PI
+        }
+        return angle/ 180
+    }
+
+        /**
+        * Convertit les radians en degrés.
+        * @param {double} angle number L'angle devant être convertit (doit être en radian).
+        * @returns number  L'équivalent en degré.
+        */
+    static ConvertToDegree(angle){
+        return angle * 180 /Math.PI
+    }
+
+    /**
+     * Vérifie si x est premier (n'ayant que 1 et lui-même pour diviseur dans N) 
+     * @param {int} x  Le nombre à tester.
+     * @returns {boolean} Si le nombre est premier
+     */
+
+    static isPremier(x){
+        x = Math.abs(x) //si x est négatif
+        
+        if (x==1) {
+            return false;
+        }
+
+        numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+        if (numbers.includes(x)) {return true;}
+        numbers.forEach(premier => {
+            if ((numbers % premier) == 0) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    /*static isCarreParfait(x){
+        return this.isEntier(Math.sqrt(x))
+    }*/
+
+    /**
+     * Vérifie si x est entier
+     * @param {Number} x Le nombre à tester 
+     * @returns {Boolean} Si le nombre est entier
+     */
+    static isEntier(x){
+        return Number.isInteger(x);
+    }
+
+    /**
+     * Logarithme en base base de a
+     * @param {Number} x Le paramètre du logarithme
+     * @param {Number} base La base du logarithme
+     * @returns {Number} Le logarithme en base base de a
+     */
+    static log(x, base){
+        const lnx = Math.log(x),        //Math.log(x) = ln x
+            lnBase  = Math.log(base);
+        
+        return lnx/lnBase;      
+        //pour tous a, b, c appartenant à C, log b(a) = log c(a)/ log c(b) 
+    }
+
+    /**
+     * Récupère tous les 2 entiers dans x tel que : 2^get2Factors(x) *k = x
+     * @param {Number} x Le nombre (Il sera arrondit au plus proche)
+     * @returns {Number}
+     */
+    static get2Factors(x){
+        x = Math.round(x);
+        return Math.floor(Math.log2(x));
+    }
+
+    /**
+     * Récupère tous les 10 entiers dans x tel que : 10^get2Factors(x) *k = x
+     * @param {Number} x Le nombre (Il sera arrondit au plus proche)
+     * @returns {Number}
+     */
+    static get10Factors(x){
+        x = Math.round(x);
+        return Math.floor(Math.log10(x));
+    }
+
+    /**
+     * Récupère tous les n entiers dans x tel que : n^get2Factors(x) *k = x
+     * @param {Number} x 
+     * @param {Number} n 
+     * @returns {Number}
+     */
+    static getNFactors(x, n){
+        return Math.floor(Mathemathics.log(x, n));
+    }
+    
+
+    /**
+     * Décompose un nombre x.
+     * @param {Number} x Le nombre à décomposer
+     * @returns {Number[]} La liste des facteurs de x, si x n'est pas entier retourne un tableau vide.
+     */
+    static decomposer(x) {          //Fonction adaptée de chatGPT
+        if (x==1 || x==0) {
+            return  [x]
+        }
+        if (x < 0) {
+            const neg = true;
+            x = abs(x);
+        }
+        if (!Number.isInteger(x)) {
+            return [];
+        }
+
+        const factors = [];
+        
+        //Récupère la puissance de 2
+        const fact2 = Mathemathics.get2Factors(x);
+        for (let i = 0; i < fact2; i++) {
+            factors.push(2);
+        }
+        
+        
+        //récupère les autres facteurs (sqrt(x) étant le plus grand facteur entier)
+        const sqrtx = Math.sqrt(x);
+        for (let i = 3; i <= sqrtx; i += 2) {
+          while (x % i === 0) {
+            factors.push(i);
+            x /= i;
+          }
+        }
+        
+        //récupère le facteur restant (si il y en a)
+        if (x > 2) {
+          factors.push(x);
+        }
+
+        if (neg) {
+            factors.push(-1);
+        }
+        
+        return factors;
+    }
+
+    /**
+     * @param  {...Number} x Les nombres à additionner
+     * @returns {Number} La somme des x
+     */
+    static sum(...x) {
+        let somme = 0; 
+        x.forEach(xn => {
+            somme += xn
+        });
+
+        return somme;
+    }
+
+    /**
+     * @param  {...Number} x Les nombres à multiplier
+     * @returns {Number} Le produit de tout les x
+     */
+    static produit(...x){
+        let product = 1;
+        x.forEach(xn => {
+            product *= xn
+        });
+
+        return product;
+    }
+
+    /**
+     * Calcule la factorielle de n SANS la fonction Gamma
+     * @param {Int} n
+     * @returns {Number} La factorielle du nombre
+     */
+    static factorial(n){
+        if (n==0) {
+            return 1;
+        }
+        for (let m = 1; m < n; m++) {
+             n *= m;
+        }
+        return m;
+    }
+}
+
+//export {Mathemathics}
---
+++ js/fractions.js
@@ -0,0 +1,145 @@
+//import { Mathemathics } from "fonctionsMath.js";
+
+
+class Fraction{
+    #numerateur = 1;
+    get #denominateur(){
+        return this.#denominateur;
+    }
+    /**
+     * set le dénominateur
+     * @param {Number} int Le nombre à assigner, int est arrondit avec Math.round()
+     */
+    set #denominateur(int){
+        int = Math.round(int);  //une fraction doit avoir des thermes entiers
+        if(int != 0){
+            this.#denominateur = int;
+        }else{
+            throw "Erreur, dénominateur à essayé d'être mis à 0"
+        }
+    }
+    
+    
+    /**
+     * Retourne la fraction numerateur/denominateur, utiliser Fraction.ToFraction() pour une fraction égale à a.
+     * @param {Int} numerateur Le haut de la fraction, la dividende
+     * @param {Int} denominateur Le bas, le diviseur
+     */
+    constructor(numerateur, denominateur){
+        if (denominateur == 0) {
+            throw "Division par 0, le dénominateur d'un objet Fraction a tenté d'être mis à 0."
+        }
+
+        if (numerateur < 0 && denominateur <  0) {
+            numerateur = Math.abs(numerateur);
+            numerateur = Math.abs(denominateur);
+        
+        }else if(denominateur < 0){
+            numerateur = -numerateur;   //on standardise en mettant le négatif au numerateur.
+            denominateur = -denominateur;
+        }
+
+        this.#numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+    
+    /**
+     * La version in-object de @see Fraction.simplifier
+     * @param {Boolean} set Si la fraction doit aussi se set à la fraction simplifiée
+     * @returns {Fraction} la fraction simplifiée
+     */
+    simplifier(set = true){
+        const f = Fraction.simplifier(this);
+        
+        if (set) {
+            this.#numerateur = f.#numerateur;
+            this.denominateur = f.#denominateur;
+        }
+        return f;
+    }
+
+    /*
+     * 
+     * @param {*} fraction 
+     */
+    /*set(fraction){
+
+    }*/
+
+    /**
+     * Simplifie un objet fraction (enlève les dénominateurs communs)
+     * @param {Fraction} fraction La fraction à simplifier
+     * @returns {Fraction} La même fraction simplifiée
+     */
+    static simplifier(fraction){
+        const factNum = Mathemathics.decomposer(fraction.#numerateur),
+            factDeno = Mathemathics.decomposer(fraction.denominateur),
+            factNumFinal = [1],
+            factDenoFinal = [1];
+        
+        //ajoute les facteurs 
+        factDeno.forEach(facteur => {
+            if(!factNum.includes(facteur)){
+                factDenoFinal.push(facteur)
+            }
+        });
+
+        factNum.forEach(facteur => {
+            if(!factDeno.includes(facteur)){
+                factNumFinal.push(facteur)
+            }
+        });
+
+        const num = Mathemathics.produit(factNumFinal),
+            deno = Mathemathics.produit(factDenoFinal);
+        
+        return new Fraction(num, deno);
+    }
+
+    /**
+     * Retourne une fraction de la forme ab/b
+     * @param {Number} a Le numérateur (est arrondit au plus proche)
+     * @param {Number} b Le dénominateur (est arrondit au plus proche)
+     * @returns {Fraction}  La fraction tel que a = c/b  
+     */
+    static ToFraction(a, b = 1){
+        a = Math.round(a),
+            b = Math.round(b);
+        
+        return new Fraction(a*b, b);
+    }
+
+    /**
+     * Retourne la valeur décimale de la fraction
+     * @param {Fraction} fraction 
+     * @returns {Fraction} La valeur décimale de fraction.
+     */
+    static ToNumber(fraction){
+        return fraction.#numerateur / fraction.denominateur;
+    }
+
+    /**
+     * Multiplie deux fractions
+     * @param {Fraction} f1 
+     * @param {Fraction} f2 
+     * @returns {Fraction}
+     */
+    static multiply(f1, f2){
+        const num = f1.#numerateur * f2.#numerateur,
+            deno = f1.denominateur * f2.denominateur;
+        
+            return new Fraction(num, deno);
+    }
+    
+    /*
+     * Ajoute deux fractions
+     * @param {Fraction} f1 
+     * @param {Fraction} f2
+     * @returns {Fraction} 
+     
+    static add(f1, f2){
+        denominateur
+    }*/
+
+}
+//export {Fraction};
