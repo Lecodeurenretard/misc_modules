f9b88674062ccc8926cdd202243389d2bfcb2800
Nils Adekambi <leretardatn@gmail.com>
2025-03-29 12:41:02 +0100

    Changed the install script in C++

--- C++/install.sh
+++ C++/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-ls
+
+if [[ ! -d "/usr/include/c++/nlibs/" ]]; then
+	echo "Creating the nlibs directory."
+	sudo mkdir /usr/include/c++/nlibs/
+	sudo ln -s /usr/include/c++/nlibs/ /usr/include/nlibs/
+fi
+
 for header in *.h *.hpp; do
 	[[ $header = "*.hpp" || $header = "*.h" ]] && continue
 	
